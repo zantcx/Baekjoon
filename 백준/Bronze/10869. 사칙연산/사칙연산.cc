c1e5a78fc5a1ec60ca966b4380c190f9495db196
zantcx <121614210+zantcx@users.noreply.github.com>
2022-12-30 02:40:04 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int A, B = 0;
+	scanf("%d %d",&A,&B);
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+	return 0;
+}
