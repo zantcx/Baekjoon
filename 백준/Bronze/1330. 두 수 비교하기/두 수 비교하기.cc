626bf7c5425701bff69b58e1801bbd2f81af6bb6
zantcx <121614210+zantcx@users.noreply.github.com>
2023-01-02 04:28:28 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	int A, B;
+	
+	scanf("%d %d", &A, &B);
+	
+	if(A > B){
+		printf(">");
+	}
+	else if(A < B){
+		printf("<");
+	}
+	else if(A = B){
+		printf("==");
+	}
+	return 0;
+}
