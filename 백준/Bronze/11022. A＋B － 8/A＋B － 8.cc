650b62a6db735d02d0e3aee2d58d1a1a62650267
zantcx <121614210+zantcx@users.noreply.github.com>
2023-01-05 03:35:58 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int T;
+	scanf("%d", &T);
+	int A[T];
+	int B[T];
+	
+	for(int i = 1; i<= T; i++){
+		scanf("%d %d", &A[i], &B[i]);
+	}
+	for(int i = 1; i<= T; i++){
+		printf("Case #%d: %d + %d = %d\n", i, A[i], B[i], A[i]+B[i]);
+	}
+	return 0;
+}
