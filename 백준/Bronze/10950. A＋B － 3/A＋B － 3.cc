cba3b0f0b22d6be4c7b38b19b8906a548c9fafe5
zantcx <121614210+zantcx@users.noreply.github.com>
2023-01-04 18:42:48 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int T;
+	scanf("%d", &T);
+	int A[T];
+	int B[T];
+	
+	for(int i=0; i < T; i++){
+		scanf("%d %d", &A[i], &B[i]);
+	}
+	for(int i=0; i < T; i++){
+		printf("%d\n", A[i]+B[i]);
+	}
+	return 0;
+}
